b9a09a6902df53e6041c6cc570cf38d2df286e8a
ianmiles <117064659+ianesteves0124@users.noreply.github.com>
2023-10-06 16:21:01 +0800

    ......

---
+++ src/pages/SeventhPage.jsx
@@ -0,0 +1,6 @@
+//DOM
+//DOM MANIPULATION === querying dom (finds an element in the dom tree)
+//DOM UPDATE === updating the dom
+//RE-RENDERING ===  after update happens it renders it on the browser or re-render
+// VIRTUAL DOM IS A COPY OF THE ORIGINAL DOM.
+// VDOM NEVER UPDATES THE ORIGINAL DOM
